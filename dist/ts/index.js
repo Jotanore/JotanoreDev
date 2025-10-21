4c9412135387a51714c2b19016936c240c5d3182
Jotanore <jotanore@hotmail.com>
2025-10-21 21:07:49 +0200

    feat: Starting functionality
    DOMContent Loaded, Menu button integration, fetch from JSON, Single Page containers visibility.

--- dist/ts/index.js
+++ dist/ts/index.js
@@ -1,5 +1,77 @@
 /* eslint-disable no-undef */
-const tres = 3;
-console.log(tres);
+console.log("tres");
+const indexButton = document.getElementById('index-button');
+const experienceButton = document.getElementById('experience-button');
+const statsButton = document.getElementById('stats-button');
+const projectsButton = document.getElementById('projects-button');
+const contactButton = document.getElementById('contact-button');
+const indexContainer = document.getElementById('index-container');
+const experienceContainer = document.getElementById('experience-container');
+const statsContainer = document.getElementById('stats-container');
+const projectsContainer = document.getElementById('projects-container');
+const contactContainer = document.getElementById('contact-container');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM cargado');
+    console.log(indexButton, experienceButton, statsButton, projectsButton, contactButton);
+    indexButton.addEventListener('click', showIndex);
+    experienceButton.addEventListener('click', showExperience);
+    statsButton.addEventListener('click', showStats);
+    projectsButton.addEventListener('click', showProjects);
+    contactButton.addEventListener('click', showContact);
+    getProjects();
+});
+function showIndex() {
+    indexContainer.classList.remove('hidden');
+    experienceContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+    console.log('index');
+}
+function showExperience() {
+    experienceContainer.classList.remove('hidden');
+    indexContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+    console.log('experience');
+}
+function showStats() {
+    statsContainer.classList.remove('hidden');
+    indexContainer.classList.add('hidden');
+    experienceContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+    console.log('experience');
+}
+function showProjects() {
+    projectsContainer.classList.remove('hidden');
+    indexContainer.classList.add('hidden');
+    experienceContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+    console.log('projects');
+}
+function showContact() {
+    contactContainer.classList.remove('hidden');
+    indexContainer.classList.add('hidden');
+    experienceContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    console.log('contact');
+}
+async function getProjects() {
+    try {
+        const response = await fetch('./src/api/projects.json');
+        if (!response.ok)
+            throw new Error('Error getting projects');
+        const projects = await response.json();
+        console.log(projects);
+        return projects;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 export {};
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,107 @@
-const tres: number = 3;
-console.log(tres);
+console.log("tres");
+
+const indexButton: HTMLButtonElement = document.getElementById('index-button') as HTMLButtonElement;
+const experienceButton: HTMLButtonElement = document.getElementById('experience-button') as HTMLButtonElement;
+const statsButton: HTMLButtonElement = document.getElementById('stats-button') as HTMLButtonElement;
+const projectsButton: HTMLButtonElement = document.getElementById('projects-button') as HTMLButtonElement;
+const contactButton: HTMLButtonElement = document.getElementById('contact-button') as HTMLButtonElement;
+
+const indexContainer: HTMLDivElement = document.getElementById('index-container') as HTMLDivElement;
+const experienceContainer: HTMLDivElement = document.getElementById('experience-container') as HTMLDivElement;
+const statsContainer: HTMLDivElement = document.getElementById('stats-container') as HTMLDivElement;
+const projectsContainer: HTMLDivElement = document.getElementById('projects-container') as HTMLDivElement;
+const contactContainer: HTMLDivElement = document.getElementById('contact-container') as HTMLDivElement;
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('DOM cargado');
+    console.log(indexButton, experienceButton, statsButton, projectsButton, contactButton);
+
+    indexButton.addEventListener('click', showIndex);
+    experienceButton.addEventListener('click', showExperience);
+    statsButton.addEventListener('click', showStats);
+    projectsButton.addEventListener('click', showProjects);
+    contactButton.addEventListener('click', showContact);
+
+    getProjects();
+    
+});
+
+function showIndex():void{
+
+    indexContainer.classList.remove('hidden');
+    
+    experienceContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+
+    console.log('index');
+}
+
+function showExperience():void{
+
+    experienceContainer.classList.remove('hidden');
+
+    indexContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+
+    console.log('experience');
+}
+
+function showStats():void{
+
+    statsContainer.classList.remove('hidden');
+
+    indexContainer.classList.add('hidden');
+    experienceContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+
+    console.log('experience');
+}
+
+function showProjects():void{
+
+    projectsContainer.classList.remove('hidden');
+
+    indexContainer.classList.add('hidden');
+    experienceContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    contactContainer.classList.add('hidden');
+
+    console.log('projects');
+}
+
+function showContact():void{
+
+    contactContainer.classList.remove('hidden');
+
+    indexContainer.classList.add('hidden');
+    experienceContainer.classList.add('hidden');
+    statsContainer.classList.add('hidden');
+    projectsContainer.classList.add('hidden');
+
+    console.log('contact');
+}
+
+async function getProjects():Promise<unknown>{
+
+    try{
+        const response = await fetch('./src/api/projects.json');
+        if (!response.ok) throw new Error('Error getting projects');
+
+        const projects = await response.json();
+        console.log(projects);
+        return projects;
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
 
